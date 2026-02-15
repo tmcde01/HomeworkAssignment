6c2afd526f9214a538cfee2654caf832924dcba3
tmcde01 <thomasmcdermott.co@gmail.com>
2026-02-15 01:52:25 +0000

    End of day update

---
+++ Transform_Silver_Logic.sql
@@ -0,0 +1,173 @@
+use database homework_assignment;
+use schema transform_silver;
+
+
+    -- Wrap everything in a procedure
+        -- Check for the new files via query
+        -- Check for the existence of tables
+        -- Add the result step logging
+    -- Create the logging table
+        -- The timestamp is the run id
+    -- Create a task dag showing workflow
+
+
+create or replace view transform_silver.vw_loan_monthly_clean as (
+-- THIS IS OUR INITIAL TABLE SCAN IN OUR CTE CASCADE.  WE COULD FILTER WITH A WHERE CLAUSE HERE, BUT FOR
+-- DEMONSTRATION PURPOSES WE WILL SHOW OUR DATA QUALITY CHECKS
+    with get_all_records as (
+        select * from raw_bronze.raw_loan_monthly
+    ),
+    -- select * from get_all_records order by "servicer_name", "loan_id", "updated_at" desc;
+    
+    -- THIS IS WHERE WE WOULD IDENTIFY AND SEPARATE OUT BAD RECORDS.  WE COULD SAVE THE RESULTS TO A FAILOVER TABLE
+    separate_bad_records as (
+        select *
+        from get_all_records
+        where 
+            "reporting_month" is null
+                or
+            "loan_id" is null
+                or 
+            "loan_id + reporting_month" is null
+                or
+            to_number("balance", 38, 2) < 0
+                or 
+            to_number(replace("interest_rate", '%', ''), 38, 2) < 0
+                or 
+            to_number(replace("interest_rate", '%', ''), 38, 2) > 25
+            ),
+    -- select * from separate_bad_records order by file_name, "servicer_name", "loan_id", "updated_at" desc;
+    
+    -- THIS IS WHERE WE WOULD IDENTIFY BAD ROW COUNTS FILES WITH BAD ROW COUNTS, USING STDDEV() AS A PRIMIITIVE MACHINE LEARNING FUNCTION
+    check_row_count as (
+        select 
+            file_unique_key,
+            file_name,
+            record_count,
+            threshold,
+            iff(record_count >= threshold,
+                   'Pass',
+                   'Fail')
+              as file_record_count_evaluation
+        from 
+            (select
+                file_unique_key,
+                file_name,
+                count(*) as record_count,
+                ceil(stddev(count(*)) over () * 2.5) as threshold
+            from get_all_records
+            group by file_name, file_unique_key
+            )
+        where file_record_count_evaluation = 'Fail'
+        ),
+    -- select * from check_row_count order by file_name;
+    
+    -- NOW WE GET TO THE "GOOD" RECORDS, THOUGH WE STILL HAVE WORK TO DO TO GET THE RIGHT ONE . . .
+    compile_good_records as (
+        select * from get_all_records 
+        where 
+            file_unique_key not in (select file_unique_key from check_row_count)
+                and
+            file_unique_key not in (select file_unique_key from separate_bad_records)
+        ),
+    -- select * from compile_good_records order by "servicer_name", "loan_id", "updated_at" desc;
+    
+    -- THIS IS THE LAST STEP IN GATHERING THE BEST RECORD FOR EACH LOAN ID FOR OUR VIEW
+    -- NOTE A CRITICAL ASSUMPION:
+        -- LOADED_AT METADATA WAS CREATED BY US.  LOGICALLLY THE MOST RECENT LOAD WOULD HAVE THE MOST RECENT RECORD.  BUT VENDOR DATA INTEGRITY IS NOT PERFECT SO WE WILL NOT USE IT
+        -- FILE_MONTH, WHICH IS ALSO FILE METADATA CREATED FROM THE FILE NAME COULD BE HELPFUL, PERHAPS EVEN THE METADATA FILE_ROW_NUMBER DEPENDING ON VENDOR BEHAVIOR
+        -- BUT LOOKING AT THE DATA, THE "updated_at" COLUMN APPEARS TO BE THE MOST SUGGESTIVE OF RECENT VENDOR ACTIVITY.  UNLIKE METADATA, IT IS NOT PROCESS DEPENDENT. 
+        -- WE DO NEED TO TALK TO OUR VENDORS TO UNDERSTAND THE BEHAVIOR, BUT HERE WE ASSUME "updated_at" TO BE THE BEST WAY TO ELIMINATE OLD AND DUPLICATE RECORDS AND GET AT 
+            -- THE FINAL RECORD WE NEED.  THIS ALSO ALLOWS BATCH INGEST OF MULTIPLE FILE DATES WITHOUT THE PERILS OF INCREMENTAL LOADS.
+            -- WE ALSO AVOID HAVING TO DO UPDATE STATEMENTS, WHICH WOULD BE ERROR-PRONE AND RESULT IN A DATA SET THAT INCLUDES OUR OWN DERIVED DATA.  FOR THIS EXERCISE, WE WANT TO AVOID THIS
+    select_best_record_for_loan_id as (
+        select *
+        from compile_good_records
+        qualify row_number() over (partition by "loan_id" order by "updated_at" desc) = 1
+    )
+    select * from select_best_record_for_loan_id order by "servicer_name", "loan_id", "updated_at" desc
+    );
+
+select * from transform_silver.vw_loan_monthly_clean; 
+
+
+declare
+    db varchar default 'HOMEWORK_ASSIGNMENT';
+    admin_schema varchar default 'ADMIN_SCHEMA';
+    control_table varchar default 'LOAN_MONTHLY_EXPECTED_FILE_SCHEMA';
+    information_schema varchar default 'INFORMATION_SCHEMA';
+    is_table varchar default 'COLUMNS';
+    source_schema varchar default 'TRANSFORM_SILVER';
+    source_table varchar default 'VW_LOAN_MONTHLY_CLEAN';
+    target_schema varchar default 'TARGET_GOLD';
+    target_table varchar default 'TARGET_LOAN_MONTHLY';
+    match_key varchar default 'FILE_RECORD_UNIQUE_KEY';
+    merge_statement_head varchar default '';
+    column_metadata_query varchar default '';
+    column_metadata resultset;
+    source_column_name varchar default '';
+    source_column_datatype_transform varchar default '';
+    target_column_name varchar default '';
+    source_column_dml varchar default '';
+    insert_dml varchar default '';
+    target_column_dml varchar default '';
+    values_dml varchar default '';
+    merge_into_statement default '';
+    
+begin
+
+    execute immediate ('use database ' || :db);
+    execute immediate ('use schema ' || :source_schema);
+
+
+    merge_statement_head := 'merge into ' || :target_schema || '.' || :target_table || ' t 
+                             using (select * from ' || :source_schema || '.' || :source_table || ' ) s
+                             on t.' || :match_key || ' = ' || 's.'|| :match_key || '
+                             when not matched then 
+                                insert(';
+
+    column_metadata_query := 'select 
+                                    act.column_name as act_column_name,
+                                    act.type as act_type,
+                                    -- USE THE EXACT SAME TRANSFORM AS WE DID WHEN CREATING THE TARGET_LOAN_MONTHLY TABLE
+                                    upper(replace(replace(act_column_name, '' '', ''_''), ''+'', ''and'')) as act_join_column_name,
+                                    isc.column_name as isc_column_name,
+                                    isc.data_type_alias as isc_data_type_alias
+                              from ' || :admin_schema || '.' || :control_table || ' act
+                              left join ' || :information_schema || '.' || :is_table || ' isc
+                                on 
+                                    act_join_column_name = isc.column_name
+                                and 
+                                    isc.table_schema = ''' || :target_schema || '''
+                                and
+                                    isc.table_name = ''' || :target_table || '''
+                              order by order_id;';
+    column_metadata := (execute immediate column_metadata_query);
+    --return table(column_metadata);
+    
+    for record in column_metadata do
+        source_column_name := 's.' || '"' || record.act_column_name || '"'; 
+        source_column_datatype_transform := record.isc_data_type_alias;
+        target_column_name := 't.' || record.isc_column_name;
+
+        source_column_dml := :source_column_name || '::' || :source_column_datatype_transform || ',' || '\n';
+        target_column_dml := :target_column_name || ',' || '\n';
+
+        values_dml := :values_dml || :source_column_dml;
+        insert_dml := :insert_dml || :target_column_dml;
+        
+    end for;
+    
+    values_dml := rtrim(values_dml, ',\n');
+    insert_dml := rtrim(insert_dml, ',\n');
+
+    merge_into_statement := :merge_statement_head || :insert_dml || ')' || '\n' || 'values(' || :values_dml || ');';
+    -- return merge_into_statement;
+    
+    execute immediate merge_into_statement;
+    
+    return (select array_agg(object_construct(*)) from table(result_scan(last_query_id())));
+    
+end;
+
+
--- database_object_creation.sql
+++ database_object_creation.sql
@@ -66,6 +66,8 @@ create file format if not exists admin_schema.infer_schema_pipe_delimited
     empty_field_as_null = true
     compression = 'gzip';
 
+-- YOU WILL NEED TO UPLOAD THE TESTING CSV FILES HERE
+
 -- CREATE A CONTROL TABLE TO BASELINE THE FILE SCHEMA 
 create table if not exists admin_schema.loan_monthly_expected_file_schema  as (
     select 
@@ -73,7 +75,7 @@ create table if not exists admin_schema.loan_monthly_expected_file_schema  as (
     from table(
             infer_schema(
                 location => '@admin_schema.testing_files',
-                file_format => 'admin_schema.infer_schema_loan_monthly',
+                file_format => 'admin_schema.infer_schema_pipe_delimited',
                 files => 'LOAN_MONTHLY_202601.csv.gz',
                 ignore_case => false
                 --max_file_count => <num>
@@ -98,6 +100,16 @@ directory = (
     refresh_on_create = true
     -- auto_refresh = true  -- Only works for external stages
     );
+
+
+-- HERE WE WILL CREATE A STREAM, THOUGH WE WILL NOT USE IT UNTIL WE PUT EVERYTHING TO A PROCEDURE
+-- -- drop stream raw_bronze.inbound_loan_monthly_files_stream;
+create stream if not exists raw_bronze.inbound_loan_monthly_files_stream
+    on stage raw_bronze.daily_files;
+-- show streams in schema raw_bronze;
+
+select * from raw_bronze.inbound_loan_monthly_files_stream;
+    
 
 -- COPY SOME FILES INTO IT
 copy files 
@@ -276,13 +288,92 @@ select * from HOMEWORK_ASSIGNMENT.RAW_BRONZE.RAW_LOAN_MONTHLY;
 -------------------------------------------------------
 use schema identifier($transform_schema);
 
--- THIS WILL BE ADDRESSED IN A SEPARATE WORKSHEET BECAUSE OF SOME OF THE COMPLEXITIES INVOLVED:
-    -- Instead of Data Quality checks, we will use filtering and failover tables to eliminate bad records
-    -- The assignment seems to be asking to create a view at the VW_LOAN_MONTHLY_CLEAN layer, to be used to merge data to the TARGET_LOAN_MONTHLY (gold) layer
-        -- The issue with this is a view is a stored query that does not persist data:  this means to load to the TARGET_LOAN_MONTHLY we would use a view to support the merge statement
-        -- Since the view will contain the transformation logic, we would apply it to every record in the RAW_LOAN_MONTHLY (bronze) layer each time the process runs.  
-        -- Thus we would be applying the same filtering and processing to the same records over and over, and as the raw layer grows the compute will grow too.
-        -- A better idea might be to persist cleaned data in a table at the LOAN_MONTHLY_CLEAN layer (no VW),  Duplicate or bad records could be purged from the raw layer, and the merge to the target layer             -- would be more performant.   
+create or replace view transform_silver.vw_loan_monthly_clean as (
+-- THIS IS OUR INITIAL TABLE SCAN IN OUR CTE CASCADE.  WE COULD FILTER WITH A WHERE CLAUSE HERE, BUT FOR
+-- DEMONSTRATION PURPOSES WE WILL SHOW OUR DATA QUALITY CHECKS
+    with get_all_records as (
+        select * from raw_bronze.raw_loan_monthly
+    ),
+    -- select * from get_all_records order by "servicer_name", "loan_id", "updated_at" desc;
+    
+    -- THIS IS WHERE WE WOULD IDENTIFY AND SEPARATE OUT BAD RECORDS.  WE COULD SAVE THE RESULTS TO A FAILOVER TABLE
+    separate_bad_records as (
+        select *
+        from get_all_records
+        where 
+            "reporting_month" is null
+                or
+            "loan_id" is null
+                or 
+            "loan_id + reporting_month" is null
+                or
+            to_number("balance", 38, 2) < 0
+                or 
+            to_number(replace("interest_rate", '%', ''), 38, 2) < 0
+                or 
+            to_number(replace("interest_rate", '%', ''), 38, 2) > 25
+            ),
+    -- select * from separate_bad_records order by file_name, "servicer_name", "loan_id", "updated_at" desc;
+    
+    -- THIS IS WHERE WE WOULD IDENTIFY BAD ROW COUNTS FILES WITH BAD ROW COUNTS, USING STDDEV() AS A PRIMIITIVE MACHINE LEARNING FUNCTION
+    check_row_count as (
+        select 
+            file_unique_key,
+            file_name,
+            record_count,
+            threshold,
+            iff(record_count >= threshold,
+                   'Pass',
+                   'Fail')
+              as file_record_count_evaluation
+        from 
+            (select
+                file_unique_key,
+                file_name,
+                count(*) as record_count,
+                ceil(stddev(count(*)) over () * 2.5) as threshold
+            from get_all_records
+            group by file_name, file_unique_key
+            )
+        where file_record_count_evaluation = 'Fail'
+        ),
+    -- select * from check_row_count order by file_name;
+    
+    -- NOW WE GET TO THE "GOOD" RECORDS, THOUGH WE STILL HAVE WORK TO DO TO GET THE RIGHT ONE . . .
+    compile_good_records as (
+        select * from get_all_records 
+        where 
+            file_unique_key not in (select file_unique_key from check_row_count)
+                and
+            file_unique_key not in (select file_unique_key from separate_bad_records)
+        ),
+    -- select * from compile_good_records order by "servicer_name", "loan_id", "updated_at" desc;
+    
+    -- THIS IS THE LAST STEP IN GATHERING THE BEST RECORD FOR EACH LOAN ID FOR OUR VIEW
+    -- NOTE A CRITICAL ASSUMPION:
+        -- LOADED_AT METADATA WAS CREATED BY US.  LOGICALLLY THE MOST RECENT LOAD WOULD HAVE THE MOST RECENT RECORD.  BUT VENDOR DATA INTEGRITY IS NOT PERFECT SO WE WILL NOT USE IT
+        -- FILE_MONTH, WHICH IS ALSO FILE METADATA CREATED FROM THE FILE NAME COULD BE HELPFUL, PERHAPS EVEN THE METADATA FILE_ROW_NUMBER DEPENDING ON VENDOR BEHAVIOR
+        -- BUT LOOKING AT THE DATA, THE "updated_at" COLUMN APPEARS TO BE THE MOST SUGGESTIVE OF RECENT VENDOR ACTIVITY.  UNLIKE METADATA, IT IS NOT PROCESS DEPENDENT. 
+        -- WE DO NEED TO TALK TO OUR VENDORS TO UNDERSTAND THE BEHAVIOR, BUT HERE WE ASSUME "updated_at" TO BE THE BEST WAY TO ELIMINATE OLD AND DUPLICATE RECORDS AND GET AT 
+            -- THE FINAL RECORD WE NEED.  THIS ALSO ALLOWS BATCH INGEST OF MULTIPLE FILE DATES WITHOUT THE PERILS OF INCREMENTAL LOADS.
+            -- WE ALSO AVOID HAVING TO DO UPDATE STATEMENTS, WHICH WOULD BE ERROR-PRONE AND RESULT IN A DATA SET THAT INCLUDES OUR OWN DERIVED DATA.  FOR THIS EXERCISE, WE WANT TO AVOID THIS
+    select_best_record_for_loan_id as (
+        -- DO A LITTLE MAGICAL IN PLACE TRANSFORM ON STATE NAMES
+        select * replace (case 
+                                when "state" = 'GUAM' then 'GU'
+                                when "state" = 'OHIO' then 'OH'
+                                when "state" = 'D.C.' then 'DC'
+                                when "state" like 'NEW %' then 'N' || substr("state", 5,1)
+                                else "state"
+                           end as "state")
+
+        from compile_good_records
+        qualify row_number() over (partition by "loan_id" order by "updated_at" desc) = 1
+    )
+    select * from select_best_record_for_loan_id order by "servicer_name", "loan_id", "updated_at" desc
+    );
+
+select * from transform_silver.vw_loan_monthly_clean; 
 
 
 
@@ -320,7 +411,7 @@ begin
     create_table_head := 'create ' || :table_type || ' table if not exists ' || :target_schema || '.' || :table_name || ' (
                                 loaded_at timestamp_tz,
                                 file_name varchar,
-                                file_month date,
+                                file_month varchar,
                                 file_unique_key varchar,
                                 file_row_number integer,
                                 file_record_unique_key varchar, ' || '\n';
@@ -352,4 +443,93 @@ begin
     
 end;
 
-select * from target_gold.target_loan_monthly;
+-- LASTLY WE CAN DO OUR MERGE
+
+declare
+    db varchar default 'HOMEWORK_ASSIGNMENT';
+    admin_schema varchar default 'ADMIN_SCHEMA';
+    control_table varchar default 'LOAN_MONTHLY_EXPECTED_FILE_SCHEMA';
+    information_schema varchar default 'INFORMATION_SCHEMA';
+    is_table varchar default 'COLUMNS';
+    source_schema varchar default 'TRANSFORM_SILVER';
+    source_table varchar default 'VW_LOAN_MONTHLY_CLEAN';
+    target_schema varchar default 'TARGET_GOLD';
+    target_table varchar default 'TARGET_LOAN_MONTHLY';
+    match_key varchar default 'FILE_RECORD_UNIQUE_KEY';
+    merge_statement_head varchar default '';
+    column_metadata_query varchar default '';
+    column_metadata resultset;
+    source_column_name varchar default '';
+    source_column_datatype_transform varchar default '';
+    target_column_name varchar default '';
+    source_column_dml varchar default '';
+    insert_dml varchar default '';
+    target_column_dml varchar default '';
+    values_dml varchar default '';
+    merge_into_statement default '';
+    
+begin
+
+    execute immediate ('use database ' || :db);
+    execute immediate ('use schema ' || :source_schema);
+
+
+    merge_statement_head := 'merge into ' || :target_schema || '.' || :target_table || ' t 
+                             using (select * from ' || :source_schema || '.' || :source_table || ' ) s
+                             on t.' || :match_key || ' = ' || 's.'|| :match_key || '
+                             when not matched then 
+                             -- WE STILL NEED OUR METADATA
+                                insert(';
+
+    column_metadata_query := 'select 
+                                    isc.column_name as isc_column_name,
+                                    isc.data_type_alias as isc_data_type_alias,
+                                    -- USE THE EXACT SAME TRANSFORM AS WE DID WHEN CREATING THE TARGET_LOAN_MONTHLY TABLE
+                                    iff(act.column_name is not null,
+                                        act.column_name,
+                                        isc.column_name) as act_column_name,
+                                    upper(replace(replace(act_column_name, '' '', ''_''), ''+'', ''and'')) as act_join_column_name,
+                                    act.type as act_type,
+                              from ' || :information_schema || '.' || :is_table || ' isc
+                              left join ' || :admin_schema || '.' || :control_table || ' act
+                                on 
+                                    isc.column_name = act_join_column_name 
+                              where 
+                                    isc.table_schema = ''' || :target_schema || '''
+                                        and
+                                    isc.table_name = ''' || :target_table || '''
+                              order by ordinal_position;';
+    column_metadata := (execute immediate column_metadata_query);
+    -- return table(column_metadata);
+    
+    for record in column_metadata do
+        target_column_name := 't.' || record.isc_column_name;
+        source_column_name := 's.' || '"' || record.act_column_name || '"'; 
+        source_column_datatype_transform := record.isc_data_type_alias;
+        
+        target_column_dml := :target_column_name || ',' || '\n';
+        source_column_dml := :source_column_name || '::' || :source_column_datatype_transform || ',' || '\n';
+        
+        insert_dml := :insert_dml || :target_column_dml;
+        values_dml := :values_dml || :source_column_dml;
+        
+    end for;
+    
+    insert_dml := rtrim(insert_dml, ',\n');
+    values_dml := rtrim(values_dml, ',\n');
+
+
+    merge_into_statement := :merge_statement_head || :insert_dml || ')' || '\n' || 'values(' || :values_dml || ');';
+    
+    execute immediate merge_into_statement;
+    
+    return (select array_agg(object_construct(*)) from table(result_scan(last_query_id())));
+    
+end;
+
+-- AND THEN CONFIRM THE RESULT:
+
+select * from target_gold.target_loan_monthly 
+order by servicer_name, updated_at, loan_id;
+
+
